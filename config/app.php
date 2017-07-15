42c05cade20532e408aa8761527b50a1afb9fd26
Jimmy <aimpeter@163.com>
2017-07-15 14:37:55 +0800

    the beian

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://pigjian.app'),
+    'url' => env('APP_URL', 'http://aimpeter.xyz'),
 
     /*
     |--------------------------------------------------------------------------
