20b173e55d64be9ab1bbeedfc11a466bcfd9ee67
Jimmy <aimpeter@163.com>
2017-07-20 17:29:26 +0800

    newest

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,7 @@
 }
 },"opacity":{"value":1,"random":true,"anim":{"enable":true,"speed":1,"opacity_min":0,"sync":false
 }
-},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":4,"size_min":0.3,"sync":false
+},"size":{"value":5,"random":true,"anim":{"enable":false,"speed":4,"size_min":0.3,"sync":false
 }
 },"line_linked":{"enable":false,"distance":150,"color":"#52697f","opacity":0.4,"width":1
 },"move":{"enable":true,"speed":1,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":600
@@ -56,7 +56,7 @@
 },"resize":true
 },"modes":{"grab":{"distance":400,"line_linked":{"opacity":1
 }
-},"bubble":{"distance":250,"size":0,"duration":2,"opacity":0,"speed":3
+},"bubble":{"distance":250,"size":3,"duration":2,"opacity":0,"speed":3
 },"repulse":{"distance":400,"duration":0.4
 },"push":{"particles_nb":4
 },"remove":{"particles_nb":2
